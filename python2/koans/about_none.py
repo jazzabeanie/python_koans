40a9e79a7bfc98817d9db2086064b8d8e5fda4cf
Jared Johnston <jazzab@gmail.com>
2015-08-30 22:25:27 +1000

    Half way through about_none.py

--- python2/koans/about_none.py
+++ python2/koans/about_none.py
@@ -12,11 +12,11 @@ class AboutNone(Koan):
 
     def test_none_is_an_object(self):
         "Unlike NULL in a lot of languages"
-        self.assertEqual(__, isinstance(None, object))
+        self.assertEqual(True, isinstance(None, object)) # so isinstance(1, 2) checks whether 1 is an type of 2, so what else can 2 be??
 
     def test_none_is_universal(self):
         "There is only one None"
-        self.assertEqual(__, None is None)
+        self.assertEqual(True, None is None) #is "is" a function?? what do you call it?apparently you use == when comparing values and is when comparing identities. An identitiy is something that is unique to every object.
 
     def test_what_exception_do_you_get_when_calling_nonexistent_methods(self):
         """
@@ -36,12 +36,15 @@ class AboutNone(Koan):
             #
             # Need a recap on how to evaluate __class__ attributes?
             #   https://github.com/gregmalcolm/python_koans/wiki/Class-Attribute
-
-            self.assertEqual(__, ex.__class__)
+            
+            # start my experiment
+            # self.assertEqual(AttributeError, labh) #should this be a NameError?? need to see if I can test for a NameError, might need a new try except statement. How do I end the current one I am in??
+            # end my experiment
+            self.assertEqual(AttributeError, ex.__class__) # When I originally tried this I got a name error because I tried it in a separate Python session where ex had not been definied. Here it's been defined with the "except Exceptions as ex:" line.
 
             # What message was attached to the exception?
             # (HINT: replace __ with part of the error message.)
-            self.assertMatch(__, ex.args[0])
+            self.assertMatch(__, ex.args[0]) #what does assertMatch do? I can't tell rom the /runner/koan.py file. I think self.args[] does someting about regular expressions.
 
     def test_none_is_distinct(self):
         """
